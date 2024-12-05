8862bfac08ff3066f3163732d02a25b21bf8e246
LaliaAzumi <laliaazumii@gmail.com>
2024-12-05 10:04:20 +0200

    update

--- event1/projet/views.py
+++ event1/projet/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
-from .models import Evenement
+from .models import Evenement, Organisateur
 from django.utils.dateparse import parse_date
 
 def creer_evenement(request):
     erreur = None
-
+    user = request.user
+        
+        # Si l'utilisateur connecté n'a pas d'organisateur associé, l'attribuer
+    organisateur, created = Organisateur.objects.get_or_create(user=user)
     if request.method == 'POST':
         titre = request.POST.get('titre')
         description = request.POST.get('description')
@@ -13,7 +16,6 @@ def creer_evenement(request):
         lieu = request.POST.get('lieu')
         capacite = request.POST.get('capacite')
         programme = request.POST.get('programe')
-        organisateur = request.POST.get('organisateur')
         image = request.FILES.get('image')
         if not titre or not date_evenement or not lieu or not organisateur:
             erreur = "Tous les champs obligatoires (*) doivent être remplis."
@@ -25,7 +27,7 @@ def creer_evenement(request):
                 date_evenement=date_evenement,
                 capacite = capacite,
                 programme = programme,
-                organisateur=organisateur,
+                id_organisateur=organisateur,
                 image = image,
             )
             evenement.save()
@@ -36,3 +38,36 @@ def creer_evenement(request):
 def liste_evenements(request):
     evenements = Evenement.objects.all()
     return render(request, 'liste_evenements.html', {'evenements': evenements})
+
+def update_evenement(request, evenement_id):
+    evenement = get_object_or_404(Evenement, id=evenement_id)
+    erreur = None
+    organisateurs = Organisateur.objects.all()
+
+    if request.method == 'POST':
+        titre = request.POST.get('titre')
+        description = request.POST.get('description')
+        date_evenement = parse_date(request.POST.get('date_evenement'))
+        lieu = request.POST.get('lieu')
+        capacite = request.POST.get('capacite')
+        programme = request.POST.get('programe')
+        organisateur = request.POST.get('organisateur')
+        image = request.FILES.get('image')
+
+        if not titre or not date_evenement or not lieu or not organisateur:
+            erreur = "Tous les champs obligatoires (*) doivent être remplis."
+        else:
+            organisateur = Organisateur.objects.get(id=organisateur)
+            evenement.titre = titre
+            evenement.description = description
+            evenement.date_evenement = date_evenement
+            evenement.lieu = lieu
+            evenement.capacite = capacite
+            evenement.programme = programme
+            evenement.organisateur = organisateur
+            if image:
+                evenement.image = image
+            evenement.save()
+            return redirect('liste_evenements')
+
+    return render(request, 'update_evenement.html', {'evenement': evenement, 'organisateurs': organisateurs, 'erreur': erreur})
