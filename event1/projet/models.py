de4980ded18a1c5307ad3bae4c8d693611fc026f
LaliaAzumi <laliaazumii@gmail.com>
2024-12-05 09:12:32 +0200

    PE-1 : model

--- event1/projet/models.py
+++ event1/projet/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.utils import timezone
+
+class Organisateur(models.Model):
+    id_organisateur = models.AutoField(db_column='ID_ORGANISATEUR', primary_key=True)  # Field name made lowercase.
+    nom_organisateur = models.TextField(db_column='NOM_ORGANISATEUR', blank=True, null=True)  # Field name made lowercase.
+    email_organisateur = models.TextField(db_column='EMAIL_ORGANISATEUR', blank=True, null=True)  # Field name made lowercase.
+    mdp_organisateur = models.TextField(db_column='MDP_ORGANISATEUR', blank=True, null=True)  # Field name made lowercase.
+    isorgnisateur = models.IntegerField(db_column='ISORGNISATEUR', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'organisateur'
+        
 class Evenement(models.Model):
     id_evenement = models.AutoField(db_column='ID_EVENEMENT', primary_key=True)  # Field name made lowercase.
-    id_organisateur = models.IntegerField(db_column='ID_ORGANISATEUR')  # Field name made lowercase.
+    id_organisateur = models.ForeignKey(Organisateur, db_column="ID_ORGANISATEUR",on_delete=models.CASCADE)  # Field name made lowercase.
     titre = models.TextField(db_column='TITRE', blank=True, null=True)  # Field name made lowercase.
     description = models.TextField(db_column='DESCRIPTION', blank=True, null=True)  # Field name made lowercase.
     lieu = models.TextField(db_column='LIEU', blank=True, null=True)  # Field name made lowercase.
@@ -16,29 +28,6 @@ class Evenement(models.Model):
         managed = False
         db_table = 'evenement'
 
-
-class Inscrire(models.Model):
-    id_evenement = models.IntegerField(db_column='ID_EVENEMENT', primary_key=True)  # Field name made lowercase. The composite primary key (ID_EVENEMENT, ID_PARTICIPANT) found, that is not supported. The first column is selected.
-    id_participant = models.IntegerField(db_column='ID_PARTICIPANT')  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'inscrire'
-        unique_together = (('id_evenement', 'id_participant'),)
-
-
-class Organisateur(models.Model):
-    id_organisateur = models.AutoField(db_column='ID_ORGANISATEUR', primary_key=True)  # Field name made lowercase.
-    nom_organisateur = models.TextField(db_column='NOM_ORGANISATEUR', blank=True, null=True)  # Field name made lowercase.
-    email_organisateur = models.TextField(db_column='EMAIL_ORGANISATEUR', blank=True, null=True)  # Field name made lowercase.
-    mdp_organisateur = models.TextField(db_column='MDP_ORGANISATEUR', blank=True, null=True)  # Field name made lowercase.
-    isorgnisateur = models.IntegerField(db_column='ISORGNISATEUR', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'organisateur'
-
-
 class Participant(models.Model):
     id_participant = models.AutoField(db_column='ID_PARTICIPANT', primary_key=True)  # Field name made lowercase.
     nom_participant = models.TextField(db_column='NOM_PARTICIPANT', blank=True, null=True)  # Field name made lowercase.
@@ -48,3 +37,17 @@ class Participant(models.Model):
 
     class Meta:
         managed = False
+
+class Inscrire(models.Model):
+    id_evenement = models.IntegerField(db_column='ID_EVENEMENT', primary_key=True)  # Field name made lowercase. The composite primary key (ID_EVENEMENT, ID_PARTICIPANT) found, that is not supported. The first column is selected.
+    id_participant = models.ForeignKey(Participant, db_column="ID_PARTICIPANT",on_delete=models.CASCADE)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'inscrire'
+        unique_together = (('id_evenement', 'id_participant'),)
+
+
+
+
+
