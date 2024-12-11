c6f3adec68a42f7f2a365af0c10f6638eb2e902c
LaliaAzumi <laliaazumii@gmail.com>
2024-12-11 12:27:24 +0200

    remove

--- event1/projet/urls.py
+++ event1/projet/urls.py
@@ -4,9 +4,8 @@ from .views import *
 from . import views
 
 urlpatterns = [
-    path('admin/',admin.site.urls),
     path('', index, name='index'),
-    path('creer_evenement.html', views.creer_evenement, name='creer_evenement'),
+    path('creer_evenement/', views.creer_evenement, name='creer_evenement'),
     path('connexion.html', views.connexion, name='connexion'),
     path('inscription.html', views.inscription, name='inscription'),
 ]
